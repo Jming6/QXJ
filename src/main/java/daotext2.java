97bda853857231a16d976117032289721b90301e
Jming6 <1143729582@qq.com>
2022-08-09 11:41:30 +0800

    Changes

---
+++ src/main/java/daotext2.java
@@ -0,0 +1,7 @@
+public class daotext2 extends daotext{
+    @Override
+    public int b(int b) {
+        int a = b+10;
+        return a;
+    }
+}
--- src/main/java/text.java
+++ src/main/java/text.java
@@ -1,7 +1,8 @@
 public class text {
     public static void main(String[] args) {
-        dao dao = new daotext();
-        String a  = "s";
+        dao dao = new daotext2();
+        System.out.println(dao.b(1));
+
         System.out.println(dao.b(2));
     }
 
