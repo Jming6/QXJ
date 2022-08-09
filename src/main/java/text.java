c4f2e102cfdebdbd9ebcdea20a582d806f54a18f
Jming6 <1143729582@qq.com>
2022-08-09 10:52:53 +0800

    Changes

--- src/main/java/text.java
+++ src/main/java/text.java
@@ -2,5 +2,6 @@ public class text {
     public static void main(String[] args) {
         System.out.print("hello 黄津耀");
         System.out.println("sb");
+        System.out.println("sb");
     }
 }
